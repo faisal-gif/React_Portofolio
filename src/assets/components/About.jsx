10d7f9b06491c4885c07a17344502568a438a8bd
faisal-gif <fagdera@gmail.com>
2024-01-09 10:41:09 +0700

    modified:   src/assets/components/About.jsx 	modified:   src/assets/css/contact.css

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -5,7 +5,10 @@ function About() {
     return (
         <section id="aboutMe">
             <span className="aboutTitle">About Me</span>
-            <span className="aboutDesc"></span>
+            <span className="aboutDesc">Saya lulusan D3 Manajemen Informatika dari Politeknik Negeri Malang dengan spesialisasi bidang pemrograman web. Senang belajar hal
+                baru dan mudah memahami hal baru serta memiliki aspirasi untuk bekerja di bidang backend web programmer,
+                mobile programmer atau IT Support. Menguasai bahasa pemrograman PHP, Java, Dart, C++ dan Menguasai
+                framework Laravel, CI,Bootrap ,React JS</span>
         </section>
     )
 }
